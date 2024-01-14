16c25d3acf3b9ff523dd671883d542d088d21f57
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2024-01-14 11:18:23 +0100

    Progress on animation loop

--- book/intro/src/main.js
+++ book/intro/src/main.js
@@ -1,8 +1,30 @@
 const canvasElement = document.createElement('canvas')
+canvasElement.width = document.body.clientWidth
+canvasElement.height = document.body.clientHeight
+
+document.body.style.margin = '0px'
 document.body.appendChild(canvasElement)
+
+
 
 const ctx = canvasElement.getContext("2d")
 
-ctx.beginPath();
-ctx.rect(20, 20, 150, 100);
-ctx.fill();
+const fps = 1000
+const dt = 1 / fps
+
+let positionX = 0
+// 50 pixels per second
+const velocityX = 50
+
+const update = () => {
+  positionX = positionX + velocityX * dt
+
+  ctx.fillStyle = 'white'
+  ctx.fillRect(0, 0, canvasElement.width, canvasElement.height)
+
+  ctx.beginPath()
+  ctx.arc(positionX, 100, 50, 0, 2 * Math.PI)
+  ctx.fillStyle = 'black'
+  ctx.fill()
+}
+setInterval(update, 1000 * dt)
